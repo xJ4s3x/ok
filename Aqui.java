0138f8d19327c5ac1995361f66b6025ddb07a6fe
Jasson A <jassonase1208@gmail.com>
2022-11-25 23:58:00 -0500

    primer commi:qwt

---
+++ Aqui.java
@@ -0,0 +1,9 @@
+public class Aqui{
+public static void main (String[] args){
+System.out.println("hola");
+System.out.println("hola2");
+System.out.println("hola3");
+System.out.println("hola4");
+System.out.println("hola5");
+}
+}
---
+++ Doc.java
@@ -0,0 +1,5 @@
+public class Doc {
+public static void main (String [] args){
+    System.out.println("Doc nuevo");
+}
+}
